be359a50f915593b9ca3238af26f7e8724dc97b3
jeongje <uonnow@gmial.com>
2020-02-02 18:55:52 +0900

    json생성

--- genie/flaskcode.py
+++ genie/flaskcode.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/test', methods=['GET'])
+def test_get():
+    rank_receive = request.args.get('rank_give')
+    rank_receive = int(rank_receive)
+    genie_info = db.genie.find_one({'rank':rank_receive},{'_id':0})
+    return jsonify({'result':'success', 'info':genie_info})
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
